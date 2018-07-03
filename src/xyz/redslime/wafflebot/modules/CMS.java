e182aee3d58b84bf0ad4a53e64b154aed86583d9
redslime <hallopiu@gmail.com>
2018-07-03 21:10:57 +0200

    Add thumbnail to CMS streams

--- src/xyz/redslime/wafflebot/modules/CMS.java
+++ src/xyz/redslime/wafflebot/modules/CMS.java
@@ -1,13 +1,15 @@
 package xyz.redslime.wafflebot.modules;
 
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IGuild;
 import xyz.redslime.wafflebot.Wafflebot;
 import xyz.redslime.wafflebot.module.ChatListenerModule;
 import xyz.redslime.wafflebot.module.annotations.Module;
 import xyz.redslime.wafflebot.module.annotations.RequireOutputChannel;
 import xyz.redslime.wafflebot.util.EmbedPresets;
 import xyz.redslime.wafflebot.util.MessageUtil;
-import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
-import sx.blah.discord.handle.obj.IGuild;
+
+import java.util.function.Function;
 
 /**
  * Created by redslime on 29.03.2018
@@ -15,6 +17,9 @@ import sx.blah.discord.handle.obj.IGuild;
 @Module
 @RequireOutputChannel
 public class CMS extends ChatListenerModule {
+
+    private static final Function<String, String> THUMBNAIL = (s) -> String.format("https://img.youtube.com/vi/%s/0.jpg", s);
+    private static final Function<String, String> REGEX = (s) -> s.replaceAll(".*/(.*)", "$1");
 
     public CMS() {
         super("CTF Match Spotlight Module", "Sends you a message when a stream is scheduled or is live in #general or the default chat", false, true);
@@ -30,7 +35,10 @@ public class CMS extends ChatListenerModule {
         if(msg.toLowerCase().contains("est") && msg.toLowerCase().contains("https://youtu.be")) {
             for(IGuild g : Wafflebot.client.getGuilds()) {
                 if(isActive(g) && !g.equals(event.getGuild())) {
-                    MessageUtil.sendMessage(Wafflebot.data.getModuleChannel(g, this), EmbedPresets.information().withTitle(getName()).withDesc(msg).withTimestamp(System.currentTimeMillis()));
+                    MessageUtil.sendMessage(Wafflebot.data.getModuleChannel(g, this),
+                            EmbedPresets.information().withTitle(getName()).withDesc(msg)
+                                    .withTimestamp(System.currentTimeMillis())
+                                    .withThumbnail(THUMBNAIL.apply(REGEX.apply(msg))));
                 }
             }
         }
